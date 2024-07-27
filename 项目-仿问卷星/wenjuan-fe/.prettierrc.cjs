886a52fa4c7beda3cd93adb82e3166d1bbbf8b27
yiTu <wangjian1423@foxmail.com>
2024-07-28 00:21:16 +0800

    init

---
+++ 项目-仿问卷星/wenjuan-fe/.prettierrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  endOfLine: 'crlf',
+  printWidth: 100,
+  proseWrap: 'preserve',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: 'es5',
+  parser: 'typescript',
+};
